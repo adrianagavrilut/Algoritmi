ff6c8dcc1c66a908fa6c74e1ec6a289d47224b2d
Adriana Gavrilut <gavrilut.adriana@student.uoradea.ro>
2023-11-28 13:42:18 +0200

    modify matrix class

--- Algoritmi/Matrix.cs
+++ Algoritmi/Matrix.cs
@@ -89,9 +89,17 @@ namespace Algoritmi
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
-                {
                     Console.Write($"{matrix[i,j]} ");
-                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void AfisareCuSpatiu(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    Console.Write(matrix[i, j] + "\t");
                 Console.WriteLine();
             }
         }
